1566f6d2eb7180f1a571c51f6d7726062cbaf7a1
Maddjakni <Maddjakni@gmail.com>
2017-05-05 12:23:46 +0000

    Add files via upload

--- 2307962549_lokgsf.sql
+++ 2307962549_lokgsf.sql
@@ -1,9 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.8
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 05, 2017 at 01:22 PM
+-- Server version: 5.7.14-log
+-- PHP Version: 5.4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
+--
+-- Database: `2307962549_lokgsf`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detailrental`
+--
 
 CREATE TABLE IF NOT EXISTS `detailrental` (
   `Rent_ID` int(11) NOT NULL,
@@ -29,10 +50,28 @@ CREATE TABLE IF NOT EXISTS `members` (
   `Street` varchar(75) NOT NULL,
   `City` varchar(75) NOT NULL,
   `State` varchar(75) NOT NULL,
-  `Zip` tinyint(3) NOT NULL,
+  `Zip` int(11) NOT NULL,
   `Balance` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`ID`, `First Name`, `Last Name`, `Street`, `City`, `State`, `Zip`, `Balance`) VALUES
+(102, 'Tami', 'Dawson', '2632 Takli Circle', 'Norene', 'TN', 37136, 11),
+(103, 'Curt', 'Knight', '4025 Cornell Court', 'Flatgap', 'KY', 41219, 6),
+(104, 'Jamal', 'Melendez', '788 East 145th Avenue', 'Quebeck', 'TN', 38579, 0),
+(105, 'Iva', 'Mcclain', '6045 Musket Ball Circle', 'Summit', 'KY', 42783, 15),
+(106, 'Miranda', 'Parks', '4469 Maxwell Place', 'Germantown', 'TN', 38183, 0),
+(107, 'Rosario', 'Elliot', '7578 Danner Avenue', 'Columbia', 'TN', 38402, 5),
+(108, 'Mattie', 'Guy', '4390 Evergreen Street', 'Lily', 'KY', 40740, 0),
+(109, 'Clint', 'Ochoa', '1711 Elm Street', 'Greeneville', 'TN', 37745, 10),
+(110, 'Lewis', 'Rosales', '4524 Southwind Circle', 'Counce', 'TN', 38326, 0),
+(111, 'Stacy', 'Mann', '2789 East Cook Avenue', 'Murfreesboro', 'TN', 37132, 8),
+(112, 'Luis', 'Trujillo', '7267 Melvin Avenue', 'Heiskell', 'TN', 37754, 3),
+(113, 'Minnie', 'Gonzales', '6430 Vasili Drive', 'Williston', 'TN', 38076, 0);
 
 -- --------------------------------------------------------
 
@@ -73,11 +112,26 @@ CREATE TABLE IF NOT EXISTS `price` (
 
 CREATE TABLE IF NOT EXISTS `rental` (
   `ID` int(11) NOT NULL,
-  `Rent_Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Rent_Date` date NOT NULL,
   `Mem_ID` int(11) NOT NULL,
   PRIMARY KEY (`ID`),
-  UNIQUE KEY `Mem_ID` (`Mem_ID`)
+  KEY `Mem_ID` (`Mem_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `rental`
+--
+
+INSERT INTO `rental` (`ID`, `Rent_Date`, `Mem_ID`) VALUES
+(1001, '2009-03-01', 103),
+(1002, '2009-03-01', 105),
+(1003, '2009-03-02', 102),
+(1004, '2009-03-02', 110),
+(1005, '2009-03-02', 111),
+(1006, '2009-03-02', 107),
+(1007, '2009-03-02', 104),
+(1008, '2009-03-03', 105),
+(1009, '2009-03-03', 111);
 
 -- --------------------------------------------------------
 
